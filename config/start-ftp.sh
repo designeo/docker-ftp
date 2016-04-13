f205250a0fb149af98d5dab56e82bd70da868dde
Tomas Polívka <tomas.polivka@designeo.cz>
2016-04-13 16:50:35 +0200

    Remove forcing port 20 + reusable

--- config/start-ftp.sh
+++ config/start-ftp.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
-adduser -D -s /sbin/nologin -h /ftp-data $FTP_USER
+if [ ! $(getent passwd $user) ] ; then
+    adduser -D -s /sbin/nologin -h /ftp-data $FTP_USER
+fi
+
 echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
 chown -R ${FTP_USER}:${FTP_USER} /ftp-data
